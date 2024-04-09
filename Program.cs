95d0af22663ba9b11463737fe525c714ee0e81c4
cielio <cielion@hotmail.com>
2024-04-09 17:22:02 -0300

    Implementacao do metodo InstalarAplicativo e testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+var nokia = new Nokia(numero: "123456", modelo: "v5", memoria: 128, iMEI: "1234432322");
+nokia.Ligar();
+nokia.InstalarAplicativo("Uber");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+var iphone = new Iphone(numero: "1223456", modelo: "I15", memoria: 128, iMEI: "43233332666");
+iphone.Ligar();
+iphone.InstalarAplicativo("FaceBook");
